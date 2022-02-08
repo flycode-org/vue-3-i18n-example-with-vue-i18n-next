79c0d8894457ed263e2bd1e49ef68146a9607aef
Gaël Thomas <gael.thomas@outlook.fr>
2022-02-08 08:33:22 +0100

    Initialize the repository

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ $t("title") }}</h1>
+  <p>{{ $t("description") }}</p>
+  <LocaleSwitcher />
+</template>
+
+<script>
+import LocaleSwitcher from "./LocaleSwitcher.vue";
+
+export default {
+  name: "HelloI18n",
+  components: { LocaleSwitcher },
+};
+</script>
---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,16 @@
+<template>
+  <select v-model="$i18n.locale">
+    <option v-for="(locale, i) in locales" :key="`locale-${i}`" :value="locale">
+      {{ locale }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "LocaleSwitcher",
+  data() {
+    return { locales: ["fr", "en"] };
+  },
+};
+</script>
